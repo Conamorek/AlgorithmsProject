a8c329f3f70339865145d40f1cf919dfa14d5ef9
Maciej_Papurzynski <maciejpap@gmail.com>
2017-12-10 23:01:21 +0100

    Bubble Sorting Class added   - bubble fixed

--- AlgoritmsProject/src/com/algoryitms/SortingByBubbleSort.java
+++ AlgoritmsProject/src/com/algoryitms/SortingByBubbleSort.java
@@ -2,12 +2,12 @@ package com.algoryitms;
 
 public class SortingByBubbleSort {
 
-    private static void swapNumbers(int firstIndex, int secondIndex, int[] arrayToBeSorted){
+    private static void swapNumbers(int secondIndex, int[] arrayToBeSorted){
 
         //Comparing two indexes in array and swaping their positions if if array[i] > array[i+1]
         int temp = arrayToBeSorted[secondIndex];
-        arrayToBeSorted[secondIndex] = arrayToBeSorted[firstIndex];
-        arrayToBeSorted[firstIndex] = temp;
+        arrayToBeSorted[secondIndex] = arrayToBeSorted[secondIndex+1];
+        arrayToBeSorted[secondIndex+1] = temp;
 
     }
 
@@ -17,7 +17,7 @@ public class SortingByBubbleSort {
         for(firstIndex = arrayToBeSorted.length-1; firstIndex > 1; firstIndex--){
             for(secondIndex = 0; secondIndex < firstIndex; secondIndex++){
                 if(arrayToBeSorted[secondIndex] > arrayToBeSorted[secondIndex + 1]){
-                    swapNumbers(firstIndex,secondIndex,arrayToBeSorted);
+                    swapNumbers(secondIndex,arrayToBeSorted);
                 }
             }
 
